8da4f2e2130cd897a8c1c01d124b463bbc002c56
AruzhanIssabekova <150043321+AruzhanIssabekova@users.noreply.github.com>
2024-05-17 21:55:15 +0500

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,12 @@ while True:
                 print("Время прихода записано")
             elif action == 'ушел':
                 corrent_time = datetime.now()
-                cur.execute(f"""INSERT INTO work_time (user_id, status, time_into) 
-                                VALUES ({user_id}, false, '{corrent_time}')""")
-                print("Время ухода записано")
+                cur.execute("""SELECT * FROM work_time WHERE user_id = %s AND status = %s ORDER BY time_into DESC LIMIT 1""", (user_id, True))
+                s = cur.fetchone()
+                if s:
+                    cur.execute("""UPDATE work_time SET status = %s, time_out = %s WHERE id = %s""", (False, corrent_time, s[0]))
+                    print("Время ухода записано")
+                else:
+                    print("Нет открытой записи для этого пользователя")
             else:
                 print("Неизвестное действие. Введите 'пришел' или 'ушел'")
